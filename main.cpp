83a4248be3dc25c5d82ec4fc5f6b3d7f4c2255c9
Tropical fish <31509619+tropicalwzc@users.noreply.github.com>
2019-12-01 20:55:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Dialog w;
-    w.setFixedHeight(798);
-    w.setFixedWidth(955);
+    w.setFixedHeight(850);
+    w.setFixedWidth(1050);
     w.setWindowFlags(w.windowFlags() | Qt::WindowMinimizeButtonHint );
     w.setWindowTitle("ice gobang");
     w.show();
